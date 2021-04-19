69179b4a5fa2cc0234e3a529435d110e73d0ab8c
daviidy <yaodavidarmel@gmail.com>
2021-04-19 23:43:31 +0000

    fixed link_tree issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.precompile += %w( apitome/*.css apitome/*.js )
+  config.assets.check_precompiled_asset = false
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
